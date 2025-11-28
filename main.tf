ad9ce6a73c4f0a689b79f581b5b1eeb5304e06b2
Divya14-10 <divyaingle007@gmail.com>
2025-11-28 16:52:35 +0530

    Update main.tf by divya

--- main.tf
+++ main.tf
@@ -14,6 +14,6 @@ provider "aws" {
 	}
 
 resource "aws_instance" "divinfra" {
-	ami = "ami-0c2b8ca1dad447f8a" 
-	instance_type = "t2.micro"
+	ami = "ami-0fa3fe0fa7920f68e" 
+	instance_type = "t3.micro"
 }
